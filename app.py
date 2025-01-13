39707de8348abfd41c9ff91d1f53eac882e355dd
LynnMuchiri <100213983+LynnMuchiri@users.noreply.github.com>
2025-01-13 19:10:25 +0300

    changes
    changes

--- app.py
+++ app.py
@@ -206,4 +206,5 @@ def stop_camera():
     return 'Camera Stopped', 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(debug=True, host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
+    # app.run(debug=True)
